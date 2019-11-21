f0beec09663b81f8dafcf71e9723d46966551575
Manoj <manojlavu@gmail.com>
2019-11-21 16:15:08 -0500

    added new comment in messages.java file

--- spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/Message.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/Message.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+//adding comment in the message java file
 package sample.atmosphere;
 
 import java.util.Date;
